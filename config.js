2d2c196beede1d5eb2c7e0592cd0beb8b6444df9
rashedin489-hash <rashedin489@gmail.com>
2026-09-22 07:00:42 +0300

    Update GEBEYA_KEY, SUPABASE_KEY, and CREATOR_CODE

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.GEBEYA_KEY = "PASTE_GEMINI_KEY";
+window.GEBEYA_KEY = "AQ.Ab8RN6JGXoJFq5wl6qkn9diXNMKinbFlm-2EanvcBWrMns4HSA";
 window.GEBEYA_SUPABASE_URL = "https://tyqjnmriqwutyrchlcit.supabase.co";
-window.GEBEYA_SUPABASE_KEY = "PASTE_SUPABASE_PUBLISHABLE_KEY";
-window.GEBEYA_CREATOR_CODE = "5150";
+window.GEBEYA_SUPABASE_KEY = "sb_publishable_CGx6jup20y2896uk-oMxXQ_dvirM39l";
+window.GEBEYA_CREATOR_CODE = "7213";
