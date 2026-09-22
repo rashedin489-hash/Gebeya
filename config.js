bf1d4fb00892783a34775043bc4594598a90393f
rashedin489-hash <rashedin489@gmail.com>
2026-09-22 06:04:46 +0300

    Add configuration variables for GEBEYA

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GEBEYA_KEY = "PASTE_GEMINI_KEY";
+window.GEBEYA_SUPABASE_URL = "https://tyqjnmriqwutyrchlcit.supabase.co";
+window.GEBEYA_SUPABASE_KEY = "PASTE_SUPABASE_PUBLISHABLE_KEY";
+window.GEBEYA_CREATOR_CODE = "5150";
